7bfd55164e14ec1bbf9e9596dcdef33c0a5ea0d6
pact0 <kuba.kowalski21331@gmail.com>
2023-09-04 19:35:19 +0200

    ccdscsd

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <QApplication>
+#include <QMainWindow>
+// #include <boost/core/addressof.hpp>
+#include <iostream>
 #include "spdlog/spdlog.h"
 
-int main(int argc, char const* argv[]) {
-    spdlog::info("Welcome to spdlog!");
-    return 0;
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    spdlog::info("Witam w spdlogu!");
+    // std::cout << boost::addressof(app) << std::endl;
+    QMainWindow mainWindow;
+    mainWindow.setWindowTitle("Moje Okno");
+    mainWindow.show();
+
+    return app.exec();
 }

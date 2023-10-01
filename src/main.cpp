258ed63155ab4b3dcaef8af02a4dd95f34a3d342
pact0 <kuba.kowalski21331@gmail.com>
2023-10-01 19:54:03 +0200

    12

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <QApplication>
 #include <QMainWindow>
 // #include <boost/core/addressof.hpp>
+#include <QAbstractOAuth2>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <iostream>
 #include "spdlog/spdlog.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
     spdlog::info("Witam w spdlogu!");
     // std::cout << boost::addressof(app) << std::endl;
     QMainWindow mainWindow;
